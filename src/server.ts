f358e9127fcdbb0b4ed2c0c6342ef2bced5851e1
Fabio Bergmann <fabiocberg@gmail.com>
2022-03-08 04:24:36 -0300

    feat: debug

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import cors from "cors";
+import debug from "debug";
 import express from "express";
+
+const log = debug("app:server");
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
@@ -8,5 +11,5 @@ app.use(cors());
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
+  log(`Server is running at port ${port}`);
 });
